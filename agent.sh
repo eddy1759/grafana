a3299552715979f8dcb575b5d7d46bea5121f547
Edet Emmanuel Asuquo <59479466+eddy1759@users.noreply.github.com>
2024-09-10 14:05:14 +0100

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -1,1 +1,16 @@
-sudo mkdir -p /etc/apt/keyrings/ && wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null && echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list && sudo apt-get update && sudo apt-get install -y grafana-agent
+#!/bin/bash
+
+# Create directory for Grafana GPG key
+sudo mkdir -p /etc/apt/keyrings/
+
+# Download and store Grafana GPG key
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+
+# Add Grafana repository
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
+
+# Update package list
+sudo apt-get update
+
+# Install Grafana Agent
+sudo apt-get install -y grafana-agent
